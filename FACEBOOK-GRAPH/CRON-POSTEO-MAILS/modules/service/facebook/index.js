900a7ab7ef6f2ff59002a4989a5f26369e2f08cb
jose <josepicoas@gmail.com>
2017-07-11 17:30:28 -0300

    optimizacion de upload al drive y publish en workplace

--- FACEBOOK-GRAPH/CRON-POSTEO-MAILS/modules/service/facebook/index.js
+++ FACEBOOK-GRAPH/CRON-POSTEO-MAILS/modules/service/facebook/index.js
@@ -11,9 +11,9 @@ var validate = require('../../utils/validate-utils')
 function postInWorkPlace(groupId, data, link) {
 
     return new Promise((resolve, reject) => {
-        
+
         const form = { message: data };
-        if(!validate.isNullOrEmptyOrUndefined(link)) form['link'] = link;
+        if (!validate.isNullOrEmptyOrUndefined(link)) form['link'] = link;
         const url = config.facebook.url + groupId + '/feed' + config.facebook.token;
 
         requester.post({ url, form }, function (err, res, body) {
@@ -30,81 +30,79 @@ function postInWorkPlace(groupId, data, link) {
 // ahora recibe attachments
 function postInWorkSpaceAttachmentForGoogle(groupId, data, attachments) {
     return new Promise((resolve) => {
-
+        console.log("archivo cargado en Drive....");
         const arrayError = [];
         const arrayResult = [];
-        // leo todos los attachments y si no es una imagen no lo proceso
-        var cantAttachments = attachments.length;
-        
-        for (let attachment of attachments) {
+        var attachment = attachments[0];
+        const cantAttachments = attachments.length;
+        const nombreAttachmentCompleto = attachment.filename.split('.');
+        const nombreAttachment = nombreAttachmentCompleto[0];
+        const extensionAttachment = nombreAttachmentCompleto[1];
+        if (cantAttachments == 1) {
+            publishAttachment(nombreAttachment, extensionAttachment, attachment.content, cantAttachments, data, groupId)
+                .then(resolve)
+                .catch(resolve)
+        } else {
             var flag = 1;
-
-
-            //obtengo el nombre del archivo y la extension
-            const nombreAttachmentCompleto = attachment.filename.split('.');
-            const nombreAttachment = nombreAttachmentCompleto[0];
-            const extensionAttachment = nombreAttachmentCompleto[1];
-
-
-            console.log("archivo cargado en Drive....");
-
-            console.log(extensionAttachment)
-
-            if (validate.ifExtensionImage(extensionAttachment)) {
-                googleServices.uploadFile(nombreAttachment, extensionAttachment, attachment.content, config.google.drive.folders.imagen)
-                    .then(imageResponse => {
-                        console.log("archivo cargado exitosamente en drive");
-
-                        var urlArray = imageResponse.webContentLink.split('&export=download');
-                        var urlImagen = urlArray[0];
-
-                        console.log(urlImagen)
-
-                        const url = config.facebook.url + groupId + '/photos' + config.facebook.token;
-
-                        const form = {
-                            message: data,
-                            url: urlImagen
-                        };
-
-
-                        requester.post({ url, form }, function (err, res, body) {
-
-                            googleServices.delefeFile(imageResponse.id);
-
-                            if (err) arrayError.push(message.error.other.generic, err);
-
-                            //TODO PROBAR PARA MAS DE DOS ARCHIVOS
-                            if (JSON.parse(body).hasOwnProperty('error')) {
-                                arrayError.push(message.error.facebook.generic, body);
-                                if (flag === cantAttachments) resolve({ error: arrayError, result: arrayResult });
-                                else flag++
-                            } else {
-                                arrayResult.push(message.success.facebook.upload.image, body);
-                                if (flag === cantAttachments) resolve({ error: arrayError, result: arrayResult });
-                                else flag++
-                            }
-
-                        });
-                    })
-
-                    .catch(err => console.log(err))
-
-            } else {
-                googleServices.uploadFile(nombreAttachment, extensionAttachment, attachment.content, config.google.drive.folders.adjunto)
-                    .then(fileResponse =>{
-                        console.log("Archivo guardado en el drive");
-                        var urlArray = fileResponse.alternateLink.split('&export=download');
-                        var urlFile = urlArray[0];
-                        postInWorkPlace(groupId, data, urlFile);        
-                    })
-            }
-
-
+            for (let attachment of attachments) {
+           
+               publishAttachment(nombreAttachment, extensionAttachment, attachment.content, cantAttachments, data, groupId)
+               .then(result =>{
+                   console.log('lo hice');
+                   return -1
+               })
+               .cath(err =>{
+                   console.log('no lo hice');
+                   return -1
+               })
+               console.log(flag++)
+          }
         }
 
+    })
+}
 
 
+function publishAttachment(nombreAttachment, extensionAttachment, content, cantAttachments, data, groupId) {
+    return new Promise((resolve, reject) => {
+        if (validate.ifExtensionImage(extensionAttachment)) {
+            googleServices.uploadFile(nombreAttachment, extensionAttachment, content, config.google.drive.folders.imagen)
+                .then(imageResponse => {
+                    console.log("archivo cargado exitosamente en drive");
+
+                    var urlArray = imageResponse.webContentLink.split('&export=download');
+                    var urlImagen = urlArray[0];
+
+                    const url = config.facebook.url + groupId + '/photos' + config.facebook.token;
+
+                    const form = {
+                        message: '',
+                        url: urlImagen
+                    };
+                    if (cantAttachments === 1) form.message = data
+
+                    requester.post({ url, form }, function (err, res, body) {
+
+                        googleServices.delefeFile(imageResponse.id);
+
+                        if (err) reject(err)
+                        if (JSON.parse(body).hasOwnProperty('error')) reject(JSON.parse(body))
+                        else resolve(JSON.parse(body))
+                    });
+                })
+                .catch(reject)
+
+        } else {
+
+            googleServices.uploadFile(nombreAttachment, extensionAttachment, content, config.google.drive.folders.adjunto)
+                .then(fileResponse => {
+                    console.log("Archivo guardado en el drive");
+                    var urlArray = fileResponse.alternateLink.split('&export=download');
+                    var urlFile = urlArray[0];
+                    if (cantAttachments === 1) postInWorkPlace(groupId, data, urlFile).then(resolve).catch(reject)
+                    else postInWorkPlace(groupId, '', urlFile).then(resolve).catch(reject)
+                })
+        }
     })
 }
 
